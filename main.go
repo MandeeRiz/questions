22b2081582aba0c77edda3dfdafab8dc97ad14d2
Amanda Erhart <erhart.a@c02tn69chtd6.lan>
2022-10-06 13:55:17 -0400

    get quote by id route working

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-var questionList = map[int]question{
-	1: {"How are you today?"},
-	2: {"What are you up to?"},
-	3: {"Can I come and visit?"},
-	4: {"What is your favorite color?"},
-	5: {"What do you need help with most often?"},
+var questionList = map[string]question{
+	"1": {"How are you today?"},
+	"2": {"What are you up to?"},
+	"3": {"Can I come and visit?"},
+	"4": {"What is your favorite color?"},
+	"5": {"What do you need help with most often?"},
 }
 
 type question struct {
@@ -21,9 +22,26 @@ type question struct {
 func main() {
 	router := gin.Default()
 	router.GET("/", getAllQuestions)
+	router.GET("/:id", getQuestionByID)
 	router.Run("localhost:8080")
 }
 
 func getAllQuestions(c *gin.Context) {
 	c.JSON(http.StatusOK, questionList)
+}
+
+func getQuestionByID(c *gin.Context) {
+	params := c.Param("id")
+	//if questionList[params] doesnt exist return error
+	for k, _ := range questionList {
+		if params == k {
+			c.JSON(http.StatusOK, questionList[params])
+			return
+		}
+		if params != k {
+			fmt.Println(k)
+			fmt.Println(params)
+		}
+	}
+	c.JSON(http.StatusBadRequest, "message: id was not found")
 }
