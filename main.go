8b8dbfe29ad6d15fce8e4e77f2eb4662233db216
Amanda Erhart <erhart.a@c02tn69chtd6.lan>
2022-10-06 13:32:20 -0400

    get all route working, created map and struct for data

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+var questionList = map[int]question{
+	1: {"How are you today?"},
+	2: {"What are you up to?"},
+	3: {"Can I come and visit?"},
+	4: {"What is your favorite color?"},
+	5: {"What do you need help with most often?"},
+}
+
+type question struct {
+	Question string `json: "How are you today?"`
+}
+
 func main() {
-	fmt.Println("Hello!")
+	router := gin.Default()
+	router.GET("/", getAllQuestions)
+	router.Run("localhost:8080")
+}
+
+func getAllQuestions(c *gin.Context) {
+	c.JSON(http.StatusOK, questionList)
 }
